98ea434f75afd25e70b773bca62b849defd3742c
ManasGupta <mmanasggupta@gmail.com>
2022-02-14 03:01:40 +0530

    Added cpp file

---
+++ solver.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include <fstream>
+#define all(v) v.begin(), v.end()
+#define allr(v) v.rbegin(), v.rend()
+
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+
+    
+    
+    return 0;
+}
